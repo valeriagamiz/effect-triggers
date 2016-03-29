7daa60f5c3816363d02205b8f63c08e3a8cb842c
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-03-29 18:32:57 -0400

    YEAH 2

--- js/main.js
+++ js/main.js
@@ -3,28 +3,28 @@ var $btnMove = $('.btn-move');
 var $btnCollapse = $('.btn-collapse-expand');
 var $btnBounce = $('.btn-bounce');
 var $btnAppend = $('.btn-append');
+var $circle = $('.circle');
 
-
-$btnShow.on ('click', function (e) {
-$('.box').toggleClass('is-clicked');
+$btnShow.on('click', function (e) {
+  $('.box').toggleClass('is-clicked');
 });
 
-$btnMove.on ('click', function (e) {
-$('.diamond').toggleClass('is-clicked2');
+$btnMove.on('click', function (e) {
+  $('.diamond').toggleClass('is-clicked2');
 });
 
-$btnCollapse.on ('click', function (e) {
-$('.panel').toggleClass('is-clicked3');
+$btnCollapse.on('click', function (e) {
+  $('.panel').toggleClass('is-clicked3');
 });
 
-$btnBounce.on ('click', function (e) {
-$('.circle').addClass('is-clicked4');
+$btnBounce.on('click', function (e) {
+  $('.circle').addClass('is-clicked4');
 });
 
-$('circle').on ('animationend', function (e) {
-$('.circle').removeClass('is-clicked4');
+$circle.on('animationend', function (e) {
+  $('.circle').removeClass('is-clicked4');
 });
 
-$btnAppend.on ('click', function (e) {
-$('.list').append('<li>New list</li>');
+$btnAppend.on('click', function (e) {
+  $('.list').append('<li>New list</li>');
 });
